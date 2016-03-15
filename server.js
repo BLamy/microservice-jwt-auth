1d310ade2f928b4e03c32929adef014b911d339f
Brett Lamy <bel423@me.com>
2016-03-15 17:07:00 -0400

    /app directory now protected by JWT

--- server.js
+++ server.js
@@ -1,51 +1,42 @@
 'use strict';
 
-const koa = require('koa');
+const app = require('koa')();
 const knex = require('knex');
-const Promise = require('bluebird');
 const fs = require('fs-promise');
 const jwt = require('koa-jwt');
+const co = require('co');
+const bcrypt = require('co-bcryptjs');
 const router = require('koa-router')();
 const passport = require('koa-passport');
 const LocalStrategy = require('passport-local').Strategy;
-const bodyParser = require('koa-bodyparser');
+const koaBody = require('koa-body');
 
-const app = koa();
 const port = process.env.PORT || 3000;
 const DBNAME = './passport.sqlite';
 
-app.use(bodyParser());
-
-//addKnexToContext
-Promise.coroutine(function*() {
-  let env = process.env;
+//----------------------
+//  Add database connection to `this.knex`
+co(function* addKnexToContext() {
   let db = knex({
     client: 'sqlite3',
     connection: {
-      // host: conn.host || env.KOA_KNEX_HOST,
-      // port: conn.port || env.KOA_KNEX_PORT,
-      // user: conn.user || env.KOA_KNEX_USER,
-      // password: conn.password || env.KOA_KNEX_PASSWORD,
-      // database: conn.database || env.KOA_KNEX_DATABASE,
-      // charset: conn.charset || env.KOA_KNEX_CHARSET,
-      // ssl: conn.ssl || env.KOA_KNEX_SSL,
-      // debug: conn.debug || env.KOA_KNEX_DEBUG,
-
-      /** For SQLite 3: http://knexjs.org/#Initialize */
-      filename: env.KOA_KNEX_FILENAME || DBNAME
+      filename: process.env.DBNAME || DBNAME
     }
   });
+  try {
+    yield db.schema.createTable('user', function(table) {
+      table.increments();
+      table.string('name').unique();
+      table.string('password');
+      table.timestamps();
+    });
+  } catch (ex) {} finally {
+    app.context.knex = db;
+  }
+});
 
-  yield db.schema.createTableIfNotExists('user', function(table) {
-    table.increments();
-    table.string('name');
-    table.string('password');
-    table.timestamps();
-  });
-
-  app.context.knex = db;
-})();
-
+//----------------------
+//  Setup a local passport strategy
 (function addPassportLocalStrategy() {
   var user = {
     id: 1,
@@ -61,14 +52,20 @@ Promise.coroutine(function*() {
   });
 
   passport.use(new LocalStrategy(function(username, password, done) {
-    // retrieve user ...
-    console.log(`${username} : ${password}`);
-    if (username === 'test' && password === 'test') {
-      done(null, user)
-    } else {
-      done(null, false)
-    }
+    co(function*() {
+      let results = yield this.knex('user').where({
+        name: username
+      });
+      let user = results[0];
+
+      if (user && (yield bcrypt.compare(password, user.password))) {
+        done(null, user)
+      } else {
+        done(null, false)
+      }
+    }.bind(app.context));
   }));
+
   app.use(passport.initialize());
   app.use(passport.session());
 })();
@@ -78,37 +75,53 @@ router.get('/', function*() {
   this.body = fs.createReadStream('views/login.html');
 });
 
-router.get('/failure', function*() {
-  this.type = 'html';
-  this.body = fs.createReadStream('views/login.html');
-});
-
-router.post('/login', passport.authenticate('local', {
+router.post('/login', koaBody({
+  multipart: true
+}), passport.authenticate('local', {
   session: false
 }), function*() {
+  let claims = {
+    'username': "foo"
+  };
+  let options = {
+    algorithm: 'RS256'
+  };
   let privateKey = yield fs.readFile('demo.rsa');
-  let claims = { 'username': "foo" };
-  let options = { algorithm: 'RS256' };
   this.type = 'base64';
   this.body = jwt.sign(claims, privateKey, options);
 });
 
-router.post('register', function*() {
+router.post('/register', koaBody({
+  multipart: true
+}), function*(next) {
+  var name = this.request.body.username;
+  var salt = yield bcrypt.genSalt(10);
+  var password = yield bcrypt.hash(this.request.body.password, salt);
+  var created_at = new Date();
+  var updated_at = created_at;
 
+  if (name && password) {
+    this.body = yield this.knex('user').returning('*').insert({
+      name,
+      password,
+      created_at,
+      updated_at
+    });
+    this.status = 200;
+    return;
+  }
+  this.status = 400;
 });
 
-router.get('/app', function*() {
-  this.type = 'html';
-  this.body = fs.createReadStream('views/app.html');
-});
-
-// app.use(route.get('/:userid', function *(userid) {
-//   this.body = {
-//     profile: yield this.knex('users').where('id', userid);
-//   };
-// });
+router.get('/app', jwt({
+    secret: fs.readFileSync('demo.rsa.pub'),
+    algorithm: 'RS256'
+  }),
+  function*() {
+    this.type = 'html';
+    this.body = fs.createReadStream('views/app.html');
+  });
 
 app.use(router.routes()).use(router.allowedMethods());
-app.listen(port, () => console.log('Server listening on', port));
 
-module.exports = app;
+app.listen(port, () => console.log('Server listening on', port));
